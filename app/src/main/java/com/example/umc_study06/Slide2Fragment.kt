b97f3a04e0f4471b2bfa4debe25141d1dae7e8a0
kangyuri1114 <yuri3334@gmail.com>
2023-05-14 16:19:59 +0900

    commit 11 6주차 미션 (완)

--- app/src/main/java/com/example/umc_study06/Slide2Fragment.kt
+++ app/src/main/java/com/example/umc_study06/Slide2Fragment.kt
@@ -10,7 +10,6 @@ import android.view.ViewGroup
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [Slide2Fragment.newInstance] factory method to
