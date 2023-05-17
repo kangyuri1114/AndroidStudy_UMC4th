3ab6fbfdb014e9227966f99d19fd551f3107df1b
kangyuri1114 <yuri3334@gmail.com>
2023-05-18 00:38:32 +0900

    Coroutine 사용으로 코드 변경 타이머

--- app/src/main/java/com/example/umc_study07/MainActivity.kt
+++ app/src/main/java/com/example/umc_study07/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.umc_study07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
+import androidx.lifecycle.lifecycleScope
 import com.example.umc_study07.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding
-            by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,39 +27,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var started = false
+    private var total = 0
 
-    val SET_TIME = 51
-    val RESET = 52
-    private val handler = object: Handler() {
-        override fun handleMessage(msg: Message) { //UI에 접근 가능한 코드 블럭
-            when(msg.what){
-                SET_TIME -> { //화면을 세팅하는 것은 what이 time일 때만
-                    binding.textTimer.text = formatTime(msg.arg1)
-                }
-                RESET -> {
-                    binding.textTimer.text = "00 : 00"
-                }
-            }
-        }
-    }
     private fun start() {
-        started = true //위에서 선언한 start 변수
+        started = true
 
-        //subThread - UI 접근 금지 -> rinOnUiThread 사용
-        thread(start=true) { //thread 내 start 함수
-            var total = 0
+        lifecycleScope.launch {//코루틴 사용
             while (true) {
-                Thread.sleep(1000) //1초에 한 번씩 작동 하도록
-                if(!started) break //1초 딜레이 후 멈추는 것을 방지
+                delay(1000)
+                if (!started) break
                 total += 1
-                val msg = Message()
-                msg.what = SET_TIME
-                msg.arg1 = total //argument에 total 담음
-                handler.sendMessage(msg)
-
-//                runOnUiThread {
-//                    binding.textTimer.text = formatTime(total)
-//                }
+                updateTimer(total)
             }
         }
     }
@@ -71,13 +48,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun stop() {
         started = false
-        binding.textTimer.text = "00 : 00"
+        total = 0
+        updateTimer(total)
     }
 
-    //시간 포맷
+    private fun updateTimer(time: Int) {
+        binding.textTimer.text = formatTime(time)
+    }
+
     private fun formatTime(time: Int): String {
-        val minute = String.format("%02d", time/60) //분
-        val second = String.format("%02d", time%60) //초
+        val minute = String.format("%02d", time / 60)
+        val second = String.format("%02d", time % 60)
         return "$minute : $second"
     }
 }
