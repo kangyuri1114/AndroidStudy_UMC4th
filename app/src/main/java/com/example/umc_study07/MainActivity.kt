bff64fb8716b1ace85a80a602e3a5a0a2ec49778
kangyuri1114 <yuri3334@gmail.com>
2023-05-18 01:14:46 +0900

    Thread를 활용하여 타이머 구현 미션 1(완) - 시간이 흐를때마다 TextView 업데이트해서 남은 시간 표시하기   - 설정한 시간 (예: 30초)가 지나면 타이머 종료 알리기

--- app/src/main/java/com/example/umc_study07/MainActivity.kt
+++ app/src/main/java/com/example/umc_study07/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.example.umc_study07.databinding.ActivityMainBinding
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -11,45 +12,60 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    private var timerJob: Job? = null
+    private var remainingTime = 0 // 초기값 0으로 설정
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.editText.inputType = android.text.InputType.TYPE_CLASS_NUMBER // 숫자 입력 타입으로 설정
+
         binding.btn1.setOnClickListener {
-            start()
+            val inputTime = binding.editText.text.toString().toIntOrNull()
+
+            if (inputTime != null && inputTime > 0) {
+                remainingTime = inputTime
+                startTimer()
+            }
         }
         binding.btn2.setOnClickListener {
-            pause()
+            pauseTimer()
         }
         binding.btn3.setOnClickListener {
-            stop()
+            stopTimer()
         }
     }
 
-    private var started = false
-    private var total = 0
+    private fun startTimer() {
+        timerJob?.cancel() // 이전 타이머 작업이 실행 중인 경우 취소
 
-    private fun start() {
-        started = true
+        timerJob = lifecycleScope.launch {
 
-        lifecycleScope.launch {//코루틴 사용
-            while (true) {
+            updateTimer(remainingTime)
+
+            while (remainingTime > 0) {
                 delay(1000)
-                if (!started) break
-                total += 1
-                updateTimer(total)
+                remainingTime -= 1
+                updateTimer(remainingTime)
+
+                if(remainingTime == 0) {
+                    binding.textTimer.text = "타이머 종료"
+                }
             }
         }
     }
 
-    private fun pause() {
-        started = false
+    private fun pauseTimer() {
+        timerJob?.cancel()
     }
 
-    private fun stop() {
-        started = false
-        total = 0
-        updateTimer(total)
+    private fun stopTimer() {
+        timerJob?.cancel()
+        remainingTime = 0
+        updateTimer(remainingTime)
+        binding.textTimer.text = "타이머 종료"
+
     }
 
     private fun updateTimer(time: Int) {
@@ -59,6 +75,6 @@ class MainActivity : AppCompatActivity() {
     private fun formatTime(time: Int): String {
         val minute = String.format("%02d", time / 60)
         val second = String.format("%02d", time % 60)
-        return "$minute : $second"
+        return "$minute:$second"
     }
 }
