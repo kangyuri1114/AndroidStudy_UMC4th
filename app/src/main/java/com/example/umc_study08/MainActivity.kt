6af51a06fb8d680d474dc8263a93d68ec43aaad7
kangyuri1114 <yuri3334@gmail.com>
2023-05-25 02:19:47 +0900

    초기 메모장 (roomDB 사용 X)

--- app/src/main/java/com/example/umc_study08/MainActivity.kt
+++ app/src/main/java/com/example/umc_study08/MainActivity.kt
@@ -4,13 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.umc_study05.MemoAdapter
-import com.example.umc_study08.MemoActivity
-import com.example.umc_study08.R
 import com.example.umc_study08.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
