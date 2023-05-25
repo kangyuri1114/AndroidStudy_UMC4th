4d1e3c3d15d9d643a7493cae32ef619860972b3b
kangyuri1114 <yuri3334@gmail.com>
2023-05-25 15:32:33 +0900

    미션 1 (완)

--- app/src/main/java/com/example/umc_study08/MainActivity.kt
+++ app/src/main/java/com/example/umc_study08/MainActivity.kt
@@ -5,31 +5,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.umc_study08.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var memoAdapter: MemoAdapter
     private lateinit var memoList: MutableList<Memo>
     private lateinit var memoDatabase: MemoDatabase
-    /**
-     * ActivityResultLauncher : Activity, Fragment에서 다른 Activity 실행 후 그 결과 처리
-     * - registerForActivityResult, launch 메서드 제공
-     * registerForActivityResult : 실행 결과를 처리할 콜백 함수를 등록
-     * launch : 다른 Activity 실행
-     * */
-    private val addMemoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
 
-        //Activity 결과 정보
+    private val addMemoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             val data: Intent? = result.data
             val memoText = data?.getStringExtra("memo")
             if (memoText != null) {
                 val newMemo = Memo(content = memoText)
-                memoDatabase.memoDao().insertMemo(newMemo)
-                loadMemos()
+                lifecycleScope.launch(Dispatchers.IO) {
+                    memoDatabase.memoDao().insertMemo(newMemo)
+                    loadMemos()
+                }
             }
         }
     }
@@ -39,31 +38,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         memoDatabase = MemoDatabase.getInstance(this)
-        //List : 읽기 전용
-        //mutableListOf : 읽기 쓰기 가능
         memoList = mutableListOf()
-        memoAdapter = MemoAdapter(memoList) { memo ->
-            val memo = memoList[memo]
-            memoDatabase.memoDao().deleteMemo(memo)
-            loadMemos()        }
+        memoAdapter = MemoAdapter(memoList) { position ->
+            deleteMemo(position)
+        }
 
         binding.recyclerView.adapter = memoAdapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        //RecyclerView에 대한 레이아웃 매니저
-        /**
-         * Activity, Fragment 는 Context 상속받은 class. -> this 사용해 Activity, Fragment Instance 참조 가능
-         * */
 
         binding.addMemoButton.setOnClickListener {
             val intent = Intent(this, MemoActivity::class.java)
             addMemoLauncher.launch(intent)
         }
+
         loadMemos()
     }
+
+    private fun deleteMemo(position: Int) {
+        val memo = memoList[position]
+        lifecycleScope.launch(Dispatchers.IO) {
+            memoDatabase.memoDao().deleteMemo(memo)
+            loadMemos()
+        }
+    }
+
     private fun loadMemos() {
-        val memos = memoDatabase.memoDao().getAllMemos()
-        memoList.clear()
-        memoList.addAll(memos)
-        memoAdapter.notifyDataSetChanged()
+        lifecycleScope.launch(Dispatchers.IO) {
+            val memos = memoDatabase.memoDao().getAllMemos()
+            withContext(Dispatchers.Main) {
+                memoList.clear()
+                memoList.addAll(memos)
+                memoAdapter.notifyDataSetChanged()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/umc_study08/MemoDatabase.kt
+++ app/src/main/java/com/example/umc_study08/MemoDatabase.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-
-@Database(entities = [Memo::class], version = 2) // 변경: 버전 2로 업데이트
+@Database(entities = [Memo::class], version = 3) // 변경: 버전 3로 업데이트
 abstract class MemoDatabase : RoomDatabase() {
     abstract fun memoDao(): MemoDao
 
@@ -22,11 +23,19 @@ abstract class MemoDatabase : RoomDatabase() {
                         MemoDatabase::class.java,
                         "app-database"
                     )
-                        .fallbackToDestructiveMigration() // 변경: destructive migration 허용
+                        .addMigrations(MIGRATION_2_3) // 마이그레이션 설정 추가
                         .build()
                 }
             }
             return memoDatabase!!
+        }
+
+        // 2 버전에서 3 버전으로의 마이그레이션 정의
+        private val MIGRATION_2_3: Migration = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // 마이그레이션 로직 작성
+                // 예시: 테이블 변경 등
+            }
         }
     }
 }
